9b9427bfb4d4a73656b2c976009e1c3a4be0c9b6
irisida <20445648+irisida@users.noreply.github.com>
2021-05-30 16:17:03 +0100

    Initial Commit

---
+++ africa-swiftui/africa_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  africa_swiftuiApp.swift
+//  africa-swiftui
+//
+//  Created by ed on 30/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct africa_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
